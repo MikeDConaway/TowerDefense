58a1a30c7a7f36f899bca8690f54897d8764ad30
Mike C <mdcfreelancework@gmail.com>
2021-07-17 18:37:56 -0700

    Progress on enemy movement

---
+++ Assets/1 - Scripts/Pathing/PathFollower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    public Path path;
+    public Transform curWaypoint;
+    public Transform destination;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curWaypoint = path.wayPoints[0];
+        destination = path.wayPoints[1];
+        transform.position = curWaypoint.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toDestination = destination.position - transform.position;
+        toDestination = speed * Time.deltaTime * toDestination.normalized;
+        transform.position += toDestination;
+    }
+}
